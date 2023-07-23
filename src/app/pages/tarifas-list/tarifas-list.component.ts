3490d4f836e259134cae0fd0c884cc029d1d2491
juandtap <jtapiav4@est.ups.edu.ec>
2023-07-23 12:13:07 -0500

    cambios tabla tarifas

--- src/app/pages/tarifas-list/tarifas-list.component.ts
+++ src/app/pages/tarifas-list/tarifas-list.component.ts
@@ -1,31 +1,41 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { Router } from '@angular/router';
+import { Tarifa } from 'src/app/model/tarifa';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { TarifaService } from 'src/app/services/tarifa.service';
+import { async } from 'rxjs';
 
 @Component({
   selector: 'app-tarifas-list',
   templateUrl: './tarifas-list.component.html',
-  styleUrls: ['./tarifas-list.component.css']
+  styleUrls: ['./tarifas-list.component.css'],
+  
 })
 export class TarifasListComponent {
 
+  tarifaList: any
+  displayedColumns: string[] = ['id', 'Tarifa', 'Precio/Hora', 'Descripcion'];
+
   
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort
+
   constructor(private tarifaService: TarifaService, private router: Router){
-
-  }
-
-  dataSource = [
+      this.tarifaList =  this.tarifaService.getAll()
+    
      
-  ]
-
-  applyFilter(event: Event){
-
   }
 
-  displayedColumns: string[] = ['id', 'Tarifa', 'Precio/Hora', 'Descripcion'];
+ 
+      
+  
+
+
+
+  
 }
 
 // ejemplo completo en : https://material.angular.io/components/table/examples#table-overview
